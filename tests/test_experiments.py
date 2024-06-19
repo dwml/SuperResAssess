8c6c16249e9ef378d133c49222c4c0ecdf7f95cb
dirk <dirk.loeffen@radboudumc.nl>
2024-06-19 12:55:19 +0200

    Add functionality for multiple assessment methods

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -6,9 +6,15 @@ from superresassess.experiments import (
     _read_yaml_as_individual_experiment,
 )
 
+
+# TODO move iterations to config file
 CONTENT = """
 ---
   initial_seed: 2024
+  assessment_methods:
+    - three_way_holdout
+    - nested_cross_validation
+    - k_fold_cross_validation
 """
 
 
@@ -41,7 +47,10 @@ def test_file_creation(temp_yaml, temp_experiment_directory):
 def test_iterations(temp_yaml, temp_experiment_directory):
     """Test whether the function creates repeated experiments."""
     setup_experiments(temp_yaml, temp_experiment_directory, iterations=3)
-    assert len(list(temp_experiment_directory.glob("*.yaml"))) == 3
+
+    # mind that this assert needs to equal 9 since we have 3 iterations and
+    # 3 assessment methods
+    assert len(list(temp_experiment_directory.glob("*.yaml"))) == 9
 
 
 def test_iterations_seeding(temp_yaml, temp_experiment_directory):
@@ -52,3 +61,26 @@ def test_iterations_seeding(temp_yaml, temp_experiment_directory):
         temp_experiment_directory / "00003.yaml"
     )
     assert experiment_3_dict["seed"] == 2026  # 2024 + 2
+
+
+def test_assessment_methods(temp_yaml, temp_experiment_directory):
+    """Test whether all assessment methods are where they need to be."""
+    setup_experiments(temp_yaml, temp_experiment_directory, iterations=2)
+    experiment_1_dict = _read_yaml_as_individual_experiment(
+        temp_experiment_directory / "00001.yaml"
+    )
+    experiment_3_dict = _read_yaml_as_individual_experiment(
+        temp_experiment_directory / "00003.yaml"
+    )
+    experiment_5_dict = _read_yaml_as_individual_experiment(
+        temp_experiment_directory / "00005.yaml"
+    )
+
+    # the first one should be three way holdout
+    assert experiment_1_dict["assessment_method"] == "three_way_holdout"
+
+    # the third one should be nested cross validation
+    assert experiment_3_dict["assessment_method"] == "nested_cross_validation"
+
+    # the fifth one should be k fold cross validation
+    assert experiment_5_dict["assessment_method"] == "k_fold_cross_validation"
