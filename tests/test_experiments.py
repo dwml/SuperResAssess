40256207d5629038d204a2786d359f174fb10b59
dirk <dirk.loeffen@radboudumc.nl>
2024-06-19 12:03:00 +0200

    Refactor out temp experiment directory fixture

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -15,15 +15,20 @@ def temp_yaml(tmp_path):
     yield d
 
 
+@pytest.fixture()
+def temp_experiment_directory(tmp_path):
+    d = tmp_path / "experiments"
+    d.mkdir()
+    yield d
+
+
 def test_yaml_reading(temp_yaml):
     experiment_dict = _read_yaml_as_experiment(temp_yaml)
     assert experiment_dict["initial_seed"] == 2024
 
 
-def test_file_creation(temp_yaml, tmp_path):
+def test_file_creation(temp_yaml, temp_experiment_directory):
     """Test whether the function creates an individual experiment based on the
     configuration file."""
-    experiment_folder = tmp_path / "experiments"
-    experiment_folder.mkdir()
-    setup_experiments(temp_yaml, experiment_folder)
-    assert (experiment_folder / "00001.yaml").exists()
+    setup_experiments(temp_yaml, temp_experiment_directory)
+    assert (temp_experiment_directory / "00001.yaml").exists()
