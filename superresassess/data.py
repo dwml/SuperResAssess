d146ddfc08904469c4e10779964532aedf51e9bc
dirk <dirk.loeffen@radboudumc.nl>
2024-07-08 10:28:14 +0200

    Add data downloader

--- superresassess/data.py
+++ superresassess/data.py
@@ -1,29 +1,49 @@
 from pathlib import Path
-from typing import Optional, List
-from abc import ABC, abstractmethod
-import logging
+from typing import Optional, List, Union
+import boto3
 
 
-class AbstractClient(ABC):
+class HCP_T2W:
+    """This class contains functionality to obtain T2w images from the Human Connectome
+    Project.
 
-    @abstractmethod
-    def download_file(bucket: str, object_name: str, file_name: str) -> None:
-        pass
+    More specifically the images that are used are the T2w brain-extracted images that
+    are registered to the native T1w patient space.
 
+    Args:
+        root (str or ``pathlib.Path``): Root directory where dataset lives or will be
+            saved if download is set to True.
+        download (bool, optional): If true, downloads the dataset from the internet
+            and puts it in root directory. If dataset is already donwloaded, it is not
+            downloaded again. Mind that if you would like this class to download the
+            dataset, the end user has to setup the AWS S3 command line tool properly.
+            For more information, see:
+    """
 
-def _download_file(
-    client: AbstractClient, bucket: str, object_name: str, file_name: str
-) -> None:
-    try:
-        client.download_file(bucket, object_name, file_name)
-    except Exception as e:
-        logging.log(e)
+    bucket = "hcp-openaccess"
+    image_file = Path("../configurations/images.txt")
 
-
-class HCP:
     def __init__(
-        self, data_dir: Path, file_list: List[str], download: Optional[bool] = False
+        self,
+        root: Union[str, Path],
+        file_list: List[str],
+        download: Optional[bool] = False,
     ):
-        self.data_dir = data_dir
+        self.root = Path(root)
         self.download = download
         self.file_list = file_list
+
+        self.s3 = boto3.client("s3")
+
+        if self.download:
+            for file in self.file_list:
+                file_path = Path(file)
+                file_name = file_path.name
+                file_parents = file_path.parents[0]
+                full_target_path = self.root.joinpath(file_parents)
+                full_target_path.mkdir(parents=True, exist_ok=True)
+                self.s3.download_file(
+                    self.bucket,
+                    str(file_path),  # endpoint cant be path, must be converted to str
+                    full_target_path.joinpath(file_name),
+                )
--- tests/test_data_downloading.py
+++ tests/test_data_downloading.py
@@ -1,14 +1,14 @@
-from botocore.exceptions import ClientError
 from pathlib import Path
-import shutil
 import pytest
+from typing import List
 
-from superresassess.data import HCP, _download_file
+from superresassess.data import HCP_T2W
 
+IMAGE_LIST_FILE = "./configurations/images.txt"
 
-IMAGE_LIST = [
-    "HCP_1200/100206/T1w/T2w_acpc_dc_restore_brain.nii.gz",
-]
+IMAGE_FIRST = "HCP_1200/100206/T1w/T2w_acpc_dc_restore_brain.nii.gz"
+IMAGE_500 = "HCP_1200/199655/T1w/T2w_acpc_dc_restore_brain.nii.gz"
+IMAGE_LAST = "HCP_1200/996782/T1w/T2w_acpc_dc_restore_brain.nii.gz"
 
 NON_EXISTING_IMAGE_LIST = [
     "HCP_1200/non_sense.nii.gz",
@@ -17,49 +17,38 @@ NON_EXISTING_IMAGE_LIST = [
 TEST_NIFTI = Path("./tests/test_data/test.nii.gz")
 
 
-# For now I don't want to access aws S3 in testing, so this will suffice
-class MockS3:
-    def __init__(self, service: str):
-        _ = service
-
-    def download_file(self, bucket, object_name: str, file_name: str):
-        if not bucket == "hcp-openaccess":
-            raise ClientError(
-                {"Error": {"Code": "404", "Message": "Not Found"}}, "download_file"
-            )
-
-        test_file = Path(object_name)
-
-        if not test_file.is_file():
-            raise ClientError(
-                {"Error": {"Code": "404", "Message": "Not Found"}}, "download_file"
-            )
-
-        target_file = Path(file_name)
-        shutil.copy(test_file, target_file)
+@pytest.fixture
+def image_list() -> List[str]:
+    with open(IMAGE_LIST_FILE, "r") as f:
+        img_lst = f.read().splitlines()
+    return img_lst
 
 
-def test_hcp_setup(tmpdir):
-    hcp = HCP(tmpdir, IMAGE_LIST, download=True)
-    assert hcp.data_dir == tmpdir
-    assert hcp.file_list == IMAGE_LIST
-    assert hcp.download
+@pytest.fixture
+def first_image_list(image_list) -> List[str]:
+    return image_list[:1]
 
 
-def test_existing_file(tmp_path):
-    s3 = MockS3("s3")
-    bucket = "hcp-openaccess"
-    file_name = str(tmp_path / "test.nii.gz")
-
-    _download_file(s3, bucket, TEST_NIFTI, file_name)
-
-    assert TEST_NIFTI.is_file()
-    assert Path(file_name).is_file()
+def test_hcp_setup(tmpdir, image_list):
+    hcp = HCP_T2W(tmpdir, image_list, download=False)
+    assert hcp.root == tmpdir
+    assert hcp.file_list == image_list
+    assert not hcp.download
 
 
-def test_non_existent_bucket(tmp_path):
-    s3 = MockS3("s3")
-    file_name = str(tmp_path / "test.nii.gz")
+def test_image_list(tmpdir, image_list):
+    hcp = HCP_T2W(tmpdir, image_list, download=False)
+    assert hcp.file_list[0] == IMAGE_FIRST
+    assert hcp.file_list[499] == IMAGE_500
+    assert hcp.file_list[-1] == IMAGE_LAST
 
-    with pytest.raises(ClientError):
-        _download_file(s3, "non_existent", TEST_NIFTI, file_name)
+
+def test_download_one_image(tmpdir, first_image_list):
+    _ = HCP_T2W(tmpdir, first_image_list, download=True)
+    assert tmpdir.join(IMAGE_FIRST).exists()
+
+
+def test_download_list_images(tmpdir):
+    _ = HCP_T2W(tmpdir, [IMAGE_500, IMAGE_LAST], download=True)
+    assert tmpdir.join(IMAGE_500).exists()
+    assert tmpdir.join(IMAGE_LAST).exists()
