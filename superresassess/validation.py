f8bc830c9bf3181ef113b3e9d0c50f60eb7c3de6
Dirk Loeffen <dirk.loeffen@radboudumc.nl>
2024-07-30 14:46:03 +0200

    Add all validation implementation and testing

--- superresassess/validation.py
+++ superresassess/validation.py
@@ -1,26 +1,51 @@
 from pathlib import Path
 from lightning.pytorch.loggers import Logger
-from lightning import LightningModule, seed_everything
+from lightning import LightningModule, seed_everything, Trainer
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 from superresassess.model import ReCNNConfiguration
+from superresassess.data import get_image_loader
 from typing import Optional
+from pydantic import BaseModel
+from monai.data import DataLoader, PatchDataset, Dataset
+from monai.transforms import RandSpatialCropSamplesd
+
+
+class ValidationConfig(BaseModel):
+    seed: int
+    samples_per_image: int
+    train_batch_size: int
+    val_batch_size: int
+    train_workers: int
+    val_workers: int
+    train_roi_size: tuple[int, int, int]
+    max_epochs: int
+    learning_rate: float
+    dict_keys: tuple[str, str]
 
 
 class Validation:
     def __init__(
         self,
         model: type[LightningModule],
-        config: ReCNNConfiguration,
+        model_config: ReCNNConfiguration,
         logger: Logger,
-        seed: int,
         train_data: list[dict[[str], Path]],
         val_data: list[dict[[str], Path]],
+        validation_config: ValidationConfig,
     ):
         self.model = model
-        self.config = config
+        self.model_config = model_config
         self.logger = logger
-        self.seed = seed
+        self.seed = validation_config.seed
         self.train_data = train_data
         self.val_data = val_data
+        self.max_epochs = validation_config.max_epochs
+        self.train_cropper = RandSpatialCropSamplesd(
+            validation_config.dict_keys,
+            validation_config.train_roi_size,
+            validation_config.samples_per_image,
+        )
+        self.validation_config = validation_config
         self.instantiated_model: Optional[LightningModule] = None
 
         # Log training data
@@ -35,4 +60,50 @@ class Validation:
 
     def _setup(self) -> None:
         seed_everything(self.seed)
-        self.instantiated_model = self.model(self.config)
+        self.instantiated_model = self.model(self.model_config)
+        loader = get_image_loader(dict_keys=self.validation_config.dict_keys)
+        train_images = Dataset(self.train_data, transform=loader)
+        train_patches = PatchDataset(
+            train_images,
+            patch_func=self.train_cropper,
+            samples_per_image=self.validation_config.samples_per_image,
+        )
+        val_images = Dataset(
+            self.val_data,
+            transform=loader,
+        )
+        self.train_loader = DataLoader(
+            train_patches,
+            batch_size=self.validation_config.train_batch_size,
+            num_workers=self.validation_config.train_workers,
+        )
+        self.val_loader = DataLoader(
+            val_images,
+            batch_size=self.validation_config.val_batch_size,
+            num_workers=self.validation_config.val_workers,
+        )
+
+    def validate(self) -> None:
+        self._setup()
+        trainer = Trainer(
+            max_epochs=self.max_epochs,
+            logger=self.logger,
+            deterministic=True,
+            callbacks=[
+                EarlyStopping(monitor="validation_loss", mode="min", patience=15),
+                ModelCheckpoint(
+                    dirpath=self.logger.log_dir,
+                    filename="{epoch}-{validation_loss:0.2f}",
+                    monitor="validation_loss",
+                    save_top_k=3,
+                    mode="min",
+                ),
+            ],
+        )
+        trainer.fit(
+            self.instantiated_model,
+            train_dataloaders=self.train_loader,
+            val_dataloaders=self.val_loader,
+        )
+
+        self.best_validation_loss = trainer.checkpoint_callback.best_model_score
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,6 +1,7 @@
-from superresassess.validation import Validation
+from superresassess.validation import Validation, ValidationConfig
 from superresassess.model import LitReCNN
 from lightning.pytorch.loggers import CSVLogger
+from lightning import LightningModule
 import yaml
 import torch
 import pytest
@@ -8,24 +9,48 @@ import pytest
 
 class TestValidation:
     my_seed = 2024
+    my_max_epochs = 2
+    my_validation_config = ValidationConfig(
+        max_epochs=my_max_epochs,
+        seed=my_seed,
+        samples_per_image=200,
+        train_batch_size=32,
+        val_batch_size=1,
+        train_workers=10,
+        val_workers=10,
+        train_roi_size=(32, 32, 32),
+        learning_rate=1e-3,
+        dict_keys=("img", "lab"),
+    )
 
-    def test_logging(self, tmp_path, mock_data, mock_recnn_config):
-        """The Validation class should log the filenames to be able to double check
-        that the correct files were used."""
-        # setup data and logger
+    @pytest.fixture(scope="function")
+    def logger(self, tmp_path):
+        return CSVLogger(save_dir=tmp_path, name="test", version="iteration1")
+
+    @pytest.fixture(scope="function")
+    def train_val_data(self, mock_data):
         train_val_test_split = (0.6, 0.2, 0.2)
         train_length = int(train_val_test_split[0] * len(mock_data))
         val_length = int(train_val_test_split[1] * len(mock_data))
-        logger = CSVLogger(save_dir=tmp_path, name="test", version="iteration1")
+        return (
+            mock_data[:train_length],
+            mock_data[train_length : train_length + val_length],
+        )
+
+    def test_logging(self, tmp_path, train_val_data, mock_recnn_config, logger):
+        """The Validation class should log the filenames to be able to double check
+        that the correct files were used."""
+        # setup data and logger
+        train_data, val_data = train_val_data
 
         # this should log the file
         _ = Validation(
             model=LitReCNN,
-            config=mock_recnn_config,
+            model_config=mock_recnn_config,
             logger=logger,
-            seed=self.my_seed,
-            train_data=mock_data[:train_length],
-            val_data=mock_data[train_length : train_length + val_length],
+            train_data=train_data,
+            val_data=val_data,
+            validation_config=self.my_validation_config,
         )
 
         # Assert that contents are correct
@@ -33,41 +58,34 @@ class TestValidation:
             tmp_path.joinpath("test").joinpath("iteration1").joinpath("hparams.yaml")
         )
         contents = yaml.safe_load(yaml_path.read_text())
-        assert contents["train_data"] == [
-            str(datum) for datum in mock_data[:train_length]
-        ]
-        assert contents["val_data"] == [
-            str(datum) for datum in mock_data[train_length : train_length + val_length]
-        ]
+        assert contents["train_data"] == [str(datum) for datum in train_data]
+        assert contents["val_data"] == [str(datum) for datum in val_data]
 
     @pytest.mark.parametrize("seed", [2024, 2025, 2026])
     def test_model_initialization_is_deterministic(
-        self, tmp_path, mock_data, mock_recnn_config, seed
+        self, tmp_path, train_val_data, mock_recnn_config, seed, logger
     ):
         # setup data and logger
-        train_val_test_split = (0.6, 0.2, 0.2)
-        train_length = int(train_val_test_split[0] * len(mock_data))
-        val_length = int(train_val_test_split[1] * len(mock_data))
-        logger = CSVLogger(save_dir=tmp_path, name="test", version="iteration1")
+        train_data, val_data = train_val_data
 
         # Seting up the validators with the same seed should yield
         # models with the same initialized weights
         validator = Validation(
             model=LitReCNN,
-            config=mock_recnn_config,
+            model_config=mock_recnn_config,
             logger=logger,
-            seed=seed,
-            train_data=mock_data[:train_length],
-            val_data=mock_data[train_length : train_length + val_length],
+            train_data=train_data,
+            val_data=val_data,
+            validation_config=self.my_validation_config,
         )
         validator._setup()
         validator2 = Validation(
             model=LitReCNN,
-            config=mock_recnn_config,
+            model_config=mock_recnn_config,
             logger=logger,
-            seed=seed,
-            train_data=mock_data[:train_length],
-            val_data=mock_data[train_length : train_length + val_length],
+            train_data=train_data,
+            val_data=val_data,
+            validation_config=self.my_validation_config,
         )
         validator2._setup()
 
@@ -78,3 +96,52 @@ class TestValidation:
             torch.equal(w1, w2) for w1, w2 in zip(weights.values(), weights2.values())
         ]
         assert all(equality)
+
+    @pytest.mark.slow
+    def test_validate_creates_new_model_instance(
+        self, train_val_data, mock_recnn_config, logger
+    ):
+        # setup data, logger and validator
+        train_data, val_data = train_val_data
+        validator = Validation(
+            model=LitReCNN,
+            model_config=mock_recnn_config,
+            logger=logger,
+            train_data=train_data,
+            val_data=val_data,
+            validation_config=self.my_validation_config,
+        )
+
+        # validate model
+        validator.validate()
+
+        # assert instantiated model is not None and is a LightningModule
+        assert validator.instantiated_model is not None
+        assert isinstance(validator.instantiated_model, LightningModule)
+
+    @pytest.mark.slow
+    @pytest.mark.parametrize(
+        "seed, validation_loss",
+        [(2024, 0.48590827), (2025, 0.48010752), (2026, 0.44385132)],
+    )
+    def test_validate_gives_correct_validation_loss(
+        self, train_val_data, mock_recnn_config, seed, validation_loss, logger
+    ):
+        # setup data, logger and validator
+        validation_config = self.my_validation_config
+        validation_config.seed = seed
+        train_data, val_data = train_val_data
+        validator = Validation(
+            model=LitReCNN,
+            model_config=mock_recnn_config,
+            logger=logger,
+            train_data=train_data,
+            val_data=val_data,
+            validation_config=self.my_validation_config,
+        )
+
+        # validate model
+        validator.validate()
+
+        # assert best_validation_loss is not None
+        assert validator.best_validation_loss == pytest.approx(validation_loss)
