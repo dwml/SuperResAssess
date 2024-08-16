e1ffb7570e533930df96ed2f1909303e2ed9e9b3
Dirk Loeffen <dirk.loeffen@radboudumc.nl>
2024-08-16 11:33:16 +0200

    Fix required arguments

--- tests/test_nested_cross_validation.py
+++ tests/test_nested_cross_validation.py
@@ -9,27 +9,31 @@ class TestNestedCrossValidation:
     my_seed = 2024
     my_max_epochs = 5
     my_n_internal_images = 20
-    my_fast_validation_config = DataConfig(
+    my_fast_data_config = DataConfig(
         max_epochs=1,
         seed=my_seed,
         samples_per_image=200,
         train_batch_size=32,
         val_batch_size=1,
+        test_batch_size=1,
         train_workers=1,
         val_workers=1,
+        test_workers=1,
         train_roi_size=(32, 32, 32),
         learning_rate=1e-3,
         dict_keys=("img", "lab"),
         limit_train_batches=1,
     )
-    my_not_so_fast_validation_config = DataConfig(
+    my_not_so_fast_data_config = DataConfig(
         max_epochs=my_max_epochs,
         seed=my_seed,
         samples_per_image=200,
         train_batch_size=32,
         val_batch_size=1,
+        test_batch_size=1,
         train_workers=1,
         val_workers=1,
+        test_workers=1,
         train_roi_size=(32, 32, 32),
         learning_rate=1e-3,
         dict_keys=("img", "lab"),
@@ -37,27 +41,24 @@ class TestNestedCrossValidation:
     )
 
     @pytest.fixture
-    def assessment_config(self):
+    def assessment_config(self, tmp_path):
         return AssessmentConfig(
             train_val_test_ratio=(0.6, 0.2, 0.2),
-            validation_config=self.my_fast_validation_config,
-            test_batch_size=1,
-            test_workers=1,
+            log_path=tmp_path,
+            data_config=self.my_fast_data_config,
             n_internal_images=self.my_n_internal_images,
+            experiment_id="00001",
         )
 
     @pytest.fixture
     def nested(
         self, mock_recnn_config, mock_data_with_external, assessment_config, tmp_path
     ):
-        experiment_id = "00001"
         return NestedCrossValidation(
             LitReCNN,
             mock_recnn_config,
             mock_data_with_external,
             assessment_config,
-            root_log_dir=tmp_path,
-            experiment_id=experiment_id,
         )
 
     def test_nested_cross_validation(self, nested):
